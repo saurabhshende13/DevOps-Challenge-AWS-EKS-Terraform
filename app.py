0efc4a200ad450158627e5d395dbd7f6422fd35b
Saurabh Shende <saurabhshende38@gmail.com>
2025-05-30 13:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask, request, jsonify
-from datetime import datetime
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def get_time():
-    current_time = datetime.utcnow().isoformat() + "Z"
-    visitor_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    return jsonify({
-        "timestamp": current_time,
-        "ip": visitor_ip
-    })
+@app.route("/")
+def hello():
+    return "Hello from Flask!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
