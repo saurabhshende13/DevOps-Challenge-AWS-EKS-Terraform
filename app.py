c4447de83c46cf87fb6f3f8e6e47572cc66906fe
Saurabh Shende <saurabhshende38@gmail.com>
2025-05-31 16:38:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+from flask_cors import CORS  # <-- ADD THIS
 
 app = Flask(__name__)
+CORS(app)  # <-- ENABLE CORS
 
 @app.route("/", methods=["GET"])
 def get_time():
